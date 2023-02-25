9e2eef94a183bf76460db3701d0fcacdbfe6b2bf
Joshua Galdamez <gg0210032019@unab.edu.sv>
2023-02-25 14:22:04 -0600

    Resumen

---
+++ Practica.py
@@ -0,0 +1,50 @@
+
+import re
+import nltk
+from inscriptis import get_text
+from nltk import word_tokenize, sent_tokenize
+
+#nltk.download()
+
+text = "Born and raised in central Argentina, Messi relocated to Spain at the age of 13 to join Barcelona, for whom he made his competitive debut aged 17 in October 2004. He established himself as an integral player for the club within the next three years, and in his first uninterrupted season in 2008–09 he helped Barcelona achieve the first treble in Spanish football; that year, aged 22, Messi won his first Ballon d'Or. Three successful seasons followed, with Messi winning four consecutive Ballons d'Or, making him the first player to win the award four times. During the 2011–12 season, he set the La Liga and European records for most goals scored in a single season, while establishing himself as Barcelona's all-time top scorer. The following two seasons, Messi finished second for the Ballon d'Or behind Cristiano Ronaldo (his perceived career rival), before regaining his best form during the 2014–15 campaign, becoming the all-time top scorer in La Liga and leading Barcelona to a historic second treble, after which he was awarded a fifth Ballon d'Or in 2015. Messi assumed captaincy of Barcelona in 2018, and won a record sixth Ballon d'Or in 2019. Out of contract, he signed for Paris Saint-Germain in August 2021. An Argentine international, Messi is the country's all-time leading goalscorer and also holds the national record for appearances. At youth level, he won the 2005 FIFA World Youth Championship, finishing the tournament with both the Golden Ball and Golden Shoe, and an Olympic gold medal at the 2008 Summer Olympics. His style of play as a diminutive, left-footed dribbler drew comparisons with his compatriot Diego Maradona, who described Messi as his successor. After his senior debut in August 2005, Messi became the youngest Argentine to play and score in a FIFA World Cup (2006), and reached the final of the 2007 Copa América, where he was named young player of the tournament. As the squad's captain from August 2011, he led Argentina to three consecutive finals: the 2014 FIFA World Cup, for which he won the Golden Ball, the 2015 Copa América, winning the Golden Ball, and the 2016 Copa América. After announcing his international retirement in 2016, he reversed his decision and led his country to qualification for the 2018 FIFA World Cup, a third-place finish at the 2019 Copa América, and victory in the 2021 Copa América, while winning the Golden Ball and Golden Boot for the latter. For this achievement, Messi received a record seventh Ballon d'Or in 2021. In 2022, he led Argentina to win the 2022 FIFA World Cup, where he won a record second Golden Ball, became the first player to score at every stage of the World Cup, and broke the record for most appearances in World Cup tournaments with 26 matches played. Messi has endorsed sportswear company Adidas since 2006. According to France Football, he was the world's highest-paid footballer for five years out of six between 2009 and 2014, and was ranked the world's highest-paid athlete by Forbes in 2019 and 2022. Messi was among Time's 100 most influential people in the world in 2011 and 2012. In February 2020, he was awarded the Laureus World Sportsman of the Year, thus becoming the first footballer and the first team-sport athlete to win the award. Later that year, Messi became the second footballer and second team-sport athlete to surpass $1 billion in career earnings."
+
+
+print("###############")
+
+sentence_list = nltk.sent_tokenize(text)
+stopwords = nltk.corpus.stopwords.words('spanish')
+
+word_frequencies = {}
+for word in nltk.word_tokenize(text):
+    if word not in stopwords:
+        if word not in word_frequencies.keys():
+            word_frequencies[word] = 1
+        else:
+            word_frequencies[word] += 1
+
+
+maximum_frequency = max(word_frequencies.values())
+
+for word in word_frequencies.keys():
+    word_frequencies[word] = (word_frequencies[word]/maximum_frequency)
+
+#CALCULA LA FRASE QUE MAS SE REPITE
+sentence_scores ={}
+for sent in sentence_list:
+    for word in nltk.word_tokenize(sent.lower()):
+        if word in word_frequencies.keys():
+            if len(sent.split(' ')) < 30:
+                if sent not in  sentence_scores.keys():
+                    sentence_scores[sent] = word_frequencies[word]
+                else:
+                    sentence_scores[sent] += word_frequencies[word]
+
+#REALIZA EL RESUMEN CON LAS MEJORES FRASES
+import heapq
+summary_sentences = heapq.nlargest(7, sentence_scores, key=sentence_scores.get)
+
+summary = ' '.join(summary_sentences)
+from googletrans import Translator
+translator = Translator()
+result = translator.translate(summary ,src='en', dest="es")
+print(result.text)
